ae2127432b537f80701bb2a0159ece1ffbedcaed
TomLopez <tom_lopez@natural-solutions.eu>
2018-04-11 17:33:23 +0200

    developpment des controller et class métier

---
+++ WebApplication4/Controllers/FacturationController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class FacturationController : ApiController
+    {
+        private Devis_Entities db;
+        public FacturationController()
+        {
+            this.db = new Devis_Entities(); // interface avec la bd
+        }
+
+
+        // GET: api/Facturation
+        public IEnumerable<Facturation> Get()
+        {
+            try
+            {
+                List<Facturation> factus = db.Facturation.ToList();
+                if ((!factus.Any()) && (factus != null)) // verification de la nullité de la liste renvoyé
+                {
+                    return factus; // si c'est bon on renvoi la liste des taches
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Aucun élément dans la liste"));
+                }
+            }
+            catch(Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, e.Message));
+            }
+        }
+
+        // GET: api/Facturation/5
+        public Facturation Get(int id)
+        {
+            Facturation res = this.db.Facturation.Where(s => s.ID == id).FirstOrDefault();   // renvoi l'objet pointé par l'id pris en paramètre      
+            if (res != null)
+            {
+                return res;
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "pas d'objet pour cet ID"));
+            }
+        }
+
+        // POST: api/Facturation
+        public void Post([FromBody] Facturation factu)
+        {
+            try
+            {
+                if (factu != null)
+                {
+                    this.db.Facturation.Add(factu); // Ajout d'un nouvel objet dans la table
+                    this.db.SaveChanges(); // mise a jour de la table
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Objet source null"));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message));
+            }
+        }
+
+        // PUT: api/Facturation/5
+        public void Put(int id, [FromBody]Facturation factu)
+        {
+            try
+            {
+                if (factu != null) // si l'objet source n'est pas null => update de la base
+                {
+                    Facturation ts = db.Facturation.Where(res => res.ID == id).FirstOrDefault(); // recuperer la tache pointé par l'id pris en paramètre de la fonction
+                    db.Facturation.Attach(ts); // Faire ecouter le contexte de base de donnée sur les changements de l'objet ts 
+                    ts.Numéro = factu.Numéro; // changement des différents attribut de l'objet pointé avec les attributs de l'objet pris en paramètre
+                    ts.Mois = factu.Mois; // same
+                    ts.Montant = factu.Montant; // same
+                    ts.FK_Devis = factu.FK_Devis; // same
+                    db.SaveChanges(); // mise a jour de la table
+                }
+                else // sinon je throw une exception
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "l'objet source est vide"));
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message));
+            }
+        }
+
+        // DELETE: api/Facturation/5
+        public void Delete(int id)
+        {
+            try // vérrif si un objet a été trouvé pour l'id
+            {
+                Facturation ts = db.Facturation.Where(res => res.ID == id).FirstOrDefault(); // récupération de la tache pointé par l'id
+                db.Facturation.Attach(ts); // ecouter les changement de l'objet 
+                db.Facturation.Remove(ts); // remove l'objet ts
+                db.SaveChanges(); // mettre a jour la table
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Pas d'objet pour cet Id"));
+            }
+        }
+    }
+}
---
+++ WebApplication4/Controllers/RessourceController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    public class RessourceController : ApiController
+    {
+        private Devis_Entities db;
+        public RessourceController()
+        {
+            this.db = new Devis_Entities();
+        }
+        // GET: api/Ressource
+        public IEnumerable<Ressource> Get()
+        {
+            try
+            {
+                List<Ressource> rs = db.Ressource.ToList();
+                if ((rs.Count > 0 ) && (rs != null)) // verification de la nullité de la liste renvoyé
+                {
+                    return rs; // si c'est bon on renvoi la liste des taches
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Aucun élément dans la liste"));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, e.Message));
+            }
+        }
+
+        // GET: api/Ressource/5
+        public Ressource Get(int id)
+        {
+            Ressource res = this.db.Ressource.Where(s => s.ID == id).FirstOrDefault();   // renvoi l'objet pointé par l'id pris en paramètre      
+            if (res != null)
+            {
+                return res;
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "pas d'objet pour cet ID"));
+            }
+        }
+        [HttpGet]
+        [ActionName("tarification")]
+        //[Route("Ressource/{id}/Tarification")]
+        public List<Tarification> GetTarification(int id)
+        {
+            //Ressource res = this.db.Ressource.Where(s => s.ID == id).FirstOrDefault();   // renvoi l'objet pointé par l'id pris en paramètre      
+            List<Tarification> tar = db.Tarification_Ressource.Include("Tarification").Where(s => s.FK_Ressource == id).Select(s => s.Tarification).ToList();
+            if (tar != null)
+            {
+                return tar;
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "pas d'objet pour cet ID"));
+            }
+        }
+
+        // POST: api/Ressource
+        public void Post([FromBody]Ressource rss)
+        {
+            try
+            {
+                if (rss != null)
+                {
+                    this.db.Ressource.Add(rss); // Ajout d'un nouvel objet dans la table
+                    this.db.SaveChanges(); // mise a jour de la table
+                }
+                else
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "Objet source null"));
+                }
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message));
+            }
+        }
+
+        // PUT: api/Ressource/5
+        public void Put(int id, [FromBody]Ressource rss)
+        {
+            try
+            {
+                if (rss != null) // si l'objet source n'est pas null => update de la base
+                {
+                    Ressource rs = db.Ressource.Where(res => res.ID == id).FirstOrDefault(); // recuperer la tache pointé par l'id pris en paramètre de la fonction
+                    db.Ressource.Attach(rs); // Faire ecouter le contexte de base de donnée sur les changements de l'objet ts 
+                    rs.Name = rss.Name; // changement des différents attribut de l'objet pointé avec les attributs de l'objet pris en paramètre
+                    rs.Mail = rss.Mail; // same
+                    rs.Initial = rss.Initial; // same
+                    rs.Niveau = rss.Niveau; // same
+                    db.SaveChanges(); // mise a jour de la table
+                }
+                else // sinon je throw une exception
+                {
+                    throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.NotFound, "l'objet source est vide"));
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, e.Message));
+            }
+
+        }
+
+        // DELETE: api/Ressource/5
+        public void Delete(int id)
+        {
+            try // vérrif si un objet a été trouvé pour l'id
+            {
+                Ressource ts = db.Ressource.Where(res => res.ID == id).FirstOrDefault(); // récupération de la tache pointé par l'id
+                db.Ressource.Attach(ts); // ecouter les changement de l'objet 
+                db.Ressource.Remove(ts); // remove l'objet ts
+                db.SaveChanges(); // mettre a jour la table
+            }
+            catch (Exception e)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Pas d'objet pour cet Id"));
+            }
+        }
+    }
+}
