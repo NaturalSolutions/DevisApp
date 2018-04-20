cf880f3828b1dc67d21dc5dea00512a397ace908
Remi Guijarro <tom_lopez@natural-solutions.eu>
2018-04-20 09:49:19 +0200

    verification changement nom user

--- WebApplication4/Models/BO/DevisProcess/DevisCalculator.cs
+++ WebApplication4/Models/BO/DevisProcess/DevisCalculator.cs
@@ -85,6 +85,9 @@ namespace WebApplication4.Models.BO.DevisProcess
         }
 
 
+
+
+
         public DevisCalculator(GeneralObject myGeneralObject)
         {
             this.db = new DevisFacturationEntities();
