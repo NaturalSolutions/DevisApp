c0039c626478164df040fb28949f3471ba2b6291
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-05-25 10:24:46 +0200

    ajout du composent fileGenerator

---
+++ webIHM/billManagerApp/src/file-generator/file-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-generator',
+  templateUrl: './file-generator.component.html',
+  styleUrls: ['./file-generator.component.css']
+})
+export class FileGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
