85564db1a36d92ff6fe2cfadd5a0e9b05ede0f88
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-05-25 11:13:29 +0200

    ajout d'aspet visuel

--- webIHM/billManagerApp/src/app/app.module.ts
+++ webIHM/billManagerApp/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
+import {FileGeneratorComponent} from '../file-generator/file-generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    FileGeneratorComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webIHM/billManagerApp/src/file-generator/file-generator.component.ts
+++ webIHM/billManagerApp/src/file-generator/file-generator.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileGeneratorComponent implements OnInit {
 
-  constructor() { }
+  private divVisibility;
+  constructor() {
+    this.divVisibility = false; 
+   }
 
   ngOnInit() {
   }
+
+  displayOptions () :void {
+    let devisScope = document.getElementById('devis');
+    let factureScope = document.getElementById('facture');
+    if(this.divVisibility == false){
+      devisScope.style.visibility = "visible";
+      factureScope.style.visibility = "visible";
+      this.divVisibility = true;
+    }else{
+      devisScope.style.visibility = "hidden";
+      factureScope.style.visibility = "hidden";
+      this.divVisibility = false;
+    }
+  } 
 
 }
