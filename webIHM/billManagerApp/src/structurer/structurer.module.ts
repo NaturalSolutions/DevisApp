19d7b0584021fdf2f2811a9aa6699d904ac537db
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-05-31 17:49:27 +0200

    ajout du structurer dans le front angular

--- webIHM/billManagerApp/src/structurer/structurer.module.ts
+++ webIHM/billManagerApp/src/structurer/structurer.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http/';
 
 @NgModule({
   imports: [
@@ -7,4 +9,47 @@ import { CommonModule } from '@angular/common';
   ],
   declarations: []
 })
-export class StructurerModule { }
+export class StructurerModule {
+	constructor(private http : HttpClient){
+	}
+
+  Angularget(configUrl) {
+    return this.http.get(configUrl, {
+      headers: {
+        'X-TrackerToken': 'b4a752782f711a7c564221c2b0c2d5dc',
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+	
+	getProjetStructure(){
+		return new Promise((resolve,reject) => {
+			this.Angularget("http://localhost/DevisAPI/api/Projet/getStructure").toPromise().then((res) => {
+				resolve(res);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	}
+
+	getStoriesStructure(){
+		return new Promise((resolve,reject) => {
+			this.Angularget("http://localhost/DevisAPI/api/Stories_d/getStructure").toPromise().then((res) => {
+				resolve(res);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	}
+
+	getTasksStructure(){
+		return new Promise((resolve,reject) => {
+			this.Angularget("http://localhost/DevisAPI/api/Tasks_d/getStructure").toPromise().then((res) => {
+				resolve(res);
+			}).catch((error) => {
+				reject(error);
+			});
+		});
+	}
+
+ }
