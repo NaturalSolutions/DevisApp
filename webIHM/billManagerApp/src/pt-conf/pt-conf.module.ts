5e2cbd1d88439c87a7771c5f65ce314af41e209d
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-06-06 17:50:11 +0200

    correction et adaptation

--- webIHM/billManagerApp/src/pt-conf/pt-conf.module.ts
+++ webIHM/billManagerApp/src/pt-conf/pt-conf.module.ts
@@ -10,5 +10,5 @@ import { CommonModule } from '@angular/common';
 export class PtConfModule { 
   public ConverterProjet = {"ID":"id","Nom" : "name","Description" : "description" , "Stories" : "listeStories"};
   public ConverterStories = {"Description" : "name","Labels" : "labels","OriginalId" : "id","Owners" : "owner_ids","StartDate" : "created_at","Type" : "story_type","URL" : "url","UpdatetDate" : "updated_at","isAMO" : "AMO","Fk_Project":"project_id", "Tasks" : "listeTaches", "isBonus" : "bonus"};
-  public ConverterTasks = {"description" : "Description","duree" : "Duration","initials" : "Initials","story_id":"FK_Stories", "isFerie" : "ferie","isWeekend" : "weekend"};
+  public ConverterTasks = {"description" : "Description","duree" : "Duration","initials" : "Initials","story_id":"FK_Stories", "isFerie" : "ferie","isWE" : "weekend"};
 }
