f1df781de64fd946edee8319386052765d664bdc
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-06-25 14:18:50 +0200

    debut de desing

--- webIHM/billManagerApp/src/app/app.component.ts
+++ webIHM/billManagerApp/src/app/app.component.ts
@@ -9,4 +9,21 @@ import * as moment from 'moment';
 export class AppComponent {
   title = 'app';
   now = moment();
+
+  private menuProperty;
+
+  constructor(){
+    this.menuProperty = true;
+  }
+
+  retractMenu = (() =>{
+    let menu = document.getElementById("menu");
+    if(this.menuProperty){
+      menu.style.width = "10%";
+      this.menuProperty = false;
+    }else{
+      menu.style.width = "20%";
+      this.menuProperty = true;
+    }
+  });
 }
