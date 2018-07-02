25dfd3f21bca05891403932c3510eb1d481232e0
Remi Guijarro <remi.guijarro-espinosa@etu.univ-amu.fr>
2018-07-02 15:35:43 +0200

    added ng bootstrap and style on settings page

--- webIHM/billManagerApp/src/settings/settings.component.ts
+++ webIHM/billManagerApp/src/settings/settings.component.ts
@@ -9,6 +9,16 @@ export class SettingsComponent implements OnInit {
 
   constructor() { }
 
+
+  appendRequestedApp(idElement){
+    let elemclassapp : HTMLCollectionOf<HTMLElement> = document.getElementsByClassName('current') as HTMLCollectionOf<HTMLElement>;
+    for(var i = 0; i < elemclassapp.length; i++){
+      elemclassapp[i].style.display = "none"; // depending on what you're doing
+    }
+    let elementToShow = document.getElementById(idElement);
+    elementToShow.style.display = "inline-block";
+  }
+
   ngOnInit() {
   }
 
